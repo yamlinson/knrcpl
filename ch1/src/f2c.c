1f84ff8d7315856b9161a3b6b9eb244fe611b1cc
yamlinson <95899054+yamlinson@users.noreply.github.com>
2025-12-29 21:19:47 -0700

    feat(f2c): align output to the right

--- ch1/src/f2c.c
+++ ch1/src/f2c.c
@@ -11,7 +11,7 @@ int main() {
     fahr = lower;
     while (fahr <= upper) {
         celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%3d %6d\n", fahr, celsius);
         fahr = fahr + step;
     }
     return 0;
