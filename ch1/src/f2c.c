947471303e40efe7304797c10bfb440e6baae4f5
yamlinson <95899054+yamlinson@users.noreply.github.com>
2025-12-29 21:32:17 -0700

    feat(f2c): reverse table print order

--- ch1/src/f2c.c
+++ ch1/src/f2c.c
@@ -8,11 +8,11 @@ int main() {
     upper = 300;
     step = 20;
 
-    fahr = lower;
-    while (fahr <= upper) {
+    fahr = upper;
+    while (fahr >= lower) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr - step;
     }
     return 0;
 }
