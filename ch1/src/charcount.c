f902c699d9110bd923e108fd526d63e6cbfefbb9
yamlinson <95899054+yamlinson@users.noreply.github.com>
2025-12-31 16:03:01 -0700

    refactor(charcount): simplify loop logic

--- ch1/src/charcount.c
+++ ch1/src/charcount.c
@@ -2,10 +2,8 @@
 
 int main() {
     double nc;
-    nc = 0;
-    while (getchar() != EOF) {
-        ++nc;
-    }
+    for (nc = 0; getchar() != EOF; ++nc)
+        ;
     printf("%.0f\n", nc);
     return 0;
 }
