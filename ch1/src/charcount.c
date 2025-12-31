3a98541dfb12140859bbe27386be87ad755c661b
yamlinson <95899054+yamlinson@users.noreply.github.com>
2025-12-31 16:02:37 -0700

    feat(charcount): use double instead of long

--- ch1/src/charcount.c
+++ ch1/src/charcount.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    long nc;
+    double nc;
     nc = 0;
     while (getchar() != EOF) {
         ++nc;
     }
-    printf("%ld\n", nc);
+    printf("%.0f\n", nc);
     return 0;
 }
