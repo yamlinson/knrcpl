79936acd4229db9805cebb2b94df9857dda3359c
yamlinson <95899054+yamlinson@users.noreply.github.com>
2026-01-04 16:56:23 -0700

    refactor(f2c): abstract conversion to function

--- ch1/src/f2c.c
+++ ch1/src/f2c.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+float f2c(float fahr);
+
 int main() {
     float fahr, celsius;
     int lower, upper, step;
@@ -10,9 +12,14 @@ int main() {
 
     fahr = upper;
     while (fahr >= lower) {
-        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        celsius = f2c(fahr);
         printf("%3.0f %6.1f\n", fahr, celsius);
         fahr = fahr - step;
     }
     return 0;
+}
+
+/* f2c: converts fahrenheit temperatures to celsius */
+float f2c(float fahr) {
+    return (5.0/9.0) * (fahr - 32.0);
 }
